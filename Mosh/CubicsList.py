a67d963ebdc248d36a1529e5b3a5a7d82f118ae7
Anand <ajkadakkal@gmail.com>
2019-02-24 13:47:01 +0530

    add pyshop complete

--- Mosh/CubicsList.py
+++ Mosh/CubicsList.py
@@ -1,24 +1,33 @@
 import openpyxl as xl
+def col_to_list(col, rows):
+    list = []
+    for cell in col:
+        c = cell[0]
+        if c.value is None:
+            break
+        list.append(c.value)
+    return list
 
 
-wb=xl.load_workbook('name.xlsx')
+wb = xl.load_workbook('name.xlsx')
 # print(wb.sheetnames)
+sheet=wb.get_active_sheet()
+print(sheet.columns[1])
 
 
 
-sheet1=wb['Sheet1']
 
-number_of_rows=sheet1.max_row
 
-name=sheet1['a1':'a'+str(number_of_rows)]
-for cell in name:
-    c=cell[0]
-    print(c.value)
-print('-'*10)
+sheet1 = wb['Sheet1']
+number_of_rows = sheet1.max_row
 
-comp = sheet1['b1':'b'+str(number_of_rows)]
-for cell in comp:
-    c=cell[0]
-    if c.value is  None:
-        break
-    print(c.value)
+name_col = sheet1['a1':'a' + str(number_of_rows)]
+
+
+name_list=col_to_list(name_col,number_of_rows)
+ame_col
+comp_col = sheet1['b1':'b' + str(number_of_rows)]
+comp_list=col_to_list(comp_col,number_of_rows)
+
+print(name_list)
+print(comp_list)
--- Mosh/exceltest.py
+++ Mosh/exceltest.py
@@ -1,18 +1,19 @@
 import openpyxl as xl
-wb=xl.load_workbook('test.xlsx')
-sheet=wb['Sheet1']
-cell=sheet['a1']
-cell=sheet.cell(1,2)
+
+wb = xl.load_workbook('test.xlsx')
+sheet = wb['Sheet1']
+cell = sheet['a1']
+cell = sheet.cell(1, 2)
 # print(cell.value)
 
 # print(sheet.max_row)
 
-for row in range (2, sheet.max_row+1):
-    cell=sheet.cell(row, 2)
+for row in range(2, sheet.max_row + 1):
+    cell = sheet.cell(row, 2)
     print(cell)
     print(cell.value)
-    corrected_amount=cell.value*0.9
-    corrected_amount_cell=sheet.cell(row,3)
-    corrected_amount_cell.value=corrected_amount
+    corrected_amount = cell.value * 0.9
+    corrected_amount_cell = sheet.cell(row, 3)
+    corrected_amount_cell.value = corrected_amount
 
 wb.save('test2.xlsx')
